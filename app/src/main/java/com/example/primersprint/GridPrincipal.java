d207273fceb755211457f29df1b4f9555a0a9a93
KevinKaunan <kleintomaskevin7@gmail.com>
2020-05-14 13:13:14 +0200

    initial

---
+++ app/src/main/java/com/example/primersprint/GridPrincipal.java
@@ -0,0 +1,14 @@
+package com.example.primersprint;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GridPrincipal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grid_principal);
+    }
+}
