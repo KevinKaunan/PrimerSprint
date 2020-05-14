9b51ef7c53d6f65ab4b1cfbf4c6582b716336099
AQUIL3S <andreshermosodemendoza@gmail.com>
2020-05-14 14:38:48 +0200

    Log In 1.0

---
+++ app/src/main/java/com/example/primersprint/A01_LogIn.java
@@ -0,0 +1,14 @@
+package com.example.primersprint;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class    A01_LogIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_a01__log_in);
+    }
+}
